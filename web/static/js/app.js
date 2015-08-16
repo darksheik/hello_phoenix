8216f1728da2e78b47b36fbe31ef645e60676e5c
Don Pflaster <dpflaster@gmail.com>
2015-08-16 00:07:56 -0400

    Following example for making chat server

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -18,4 +18,29 @@ import "deps/phoenix_html/web/static/js/phoenix_html"
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
-// import socket from "./socket"
+import {Socket} from "deps/phoenix/web/static/js/phoenix"
+
+let socket = new Socket("/socket")
+socket.connect()
+let chan = socket.channel("rooms:lobby", {})
+chan.join().receive("ok", chan => {
+  console.log("Welcome to Phoenix Chat!")
+})
+
+let chatInput         = $("#chat-input")
+let messagesContainer = $("#messages")
+
+chatInput.on("keypress", event => {
+  if(event.keyCode === 13){
+    chan.push("new_msg", {body: chatInput.val()})
+    chatInput.val("")
+  }
+})
+
+chan.on("new_msg", payload => {
+  messagesContainer.append(`<br/>[${Date()}] ${payload.body}`)
+})
+
+chan.join().receive("ok", chan => {
+  console.log("Welcome to Phoenix Chat!")
+})
