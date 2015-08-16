0268a05397c64ac068760a5662988e538c13900b
Don Pflaster <dpflaster@gmail.com>
2015-08-16 09:17:07 -0400

    Oops remove duplicate join

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -23,9 +23,6 @@ import {Socket} from "deps/phoenix/web/static/js/phoenix"
 let socket = new Socket("/socket")
 socket.connect()
 let chan = socket.channel("rooms:lobby", {})
-chan.join().receive("ok", chan => {
-  console.log("Welcome to Phoenix Chat!")
-})
 
 let chatInput         = $("#chat-input")
 let messagesContainer = $("#messages")
